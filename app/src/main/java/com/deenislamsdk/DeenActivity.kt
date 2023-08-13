aedb4b294b5f66acd11bd1de5b7cb7c7317ca72d
Rabiul Hasan <rabiul.gakk@gmail.com>
2023-08-13 12:45:50 +0600

    - Deen SDK sample

---
+++ app/src/main/java/com/deenislamsdk/DeenActivity.kt
@@ -0,0 +1,87 @@
+package com.deenislamsdk
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+
+class DeenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val msisdn:EditText = findViewById(R.id.phone_number)
+        val authBtn:AppCompatButton = findViewById(R.id.login)
+        val tasbeehBtn:AppCompatButton = findViewById(R.id.tasbeeh)
+        val forbiddenBtn:AppCompatButton = findViewById(R.id.prayertime)
+        val prayernotifyon:AppCompatButton = findViewById(R.id.prayernotifyon)
+        val prayernotifyoff:AppCompatButton = findViewById(R.id.prayernotifyoff)
+
+       /* authBtn.setOnClickListener {
+            if(msisdn.text.isNotEmpty()){
+                Deen.openDeen(this,msisdn.text.toString(),this@DeenActivity)
+            }else{
+                Toast.makeText(this,"Enter number", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        tasbeehBtn.setOnClickListener {
+            if(msisdn.text.isNotEmpty()){
+                Deen.openTasbeeh(this,msisdn.text.toString(),this@DeenActivity)
+            }else{
+                Toast.makeText(this,"Enter number", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        forbiddenBtn.setOnClickListener {
+            if(msisdn.text.isNotEmpty()){
+                Deen.openPrayerTime(this,msisdn.text.toString(),this@DeenActivity)
+            }else{
+                Toast.makeText(this,"Enter number", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        prayernotifyon.setOnClickListener {
+            if(msisdn.text.isNotEmpty()){
+                Deen.prayerNotification(true,this,msisdn.text.toString(),this@DeenActivity)
+            }else{
+                Toast.makeText(this,"Enter number", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        prayernotifyoff.setOnClickListener {
+            if(msisdn.text.isNotEmpty()){
+                Deen.prayerNotification(false,this,msisdn.text.toString(),this@DeenActivity)
+            }else{
+                Toast.makeText(this,"Enter number", Toast.LENGTH_SHORT).show()
+            }
+        }*/
+
+    }
+
+   /* override fun onDestroy() {
+        super.onDestroy()
+        Deen.destroySDK()
+    }
+
+    override fun onAuthSuccess() {
+        Toast.makeText(this, "Auth Success Callback", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onAuthFailed() {
+        Toast.makeText(this, "Auth Failed Callback", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun prayerNotificationOn() {
+        Toast.makeText(this, "Prayer notification enable Callback", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun prayerNotificationOff() {
+        Toast.makeText(this, "Prayer notification disable Callback", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun prayerNotificationFailed() {
+        Toast.makeText(this, "Prayer notification failed Callback", Toast.LENGTH_SHORT).show()
+    }*/
+}
